2faa5a992fed6fb2e82641665d6fa66c280b08b1
Eduardo <eduardojvr10@gmail.com>
2017-06-19 21:24:24 -0300

    Inserção de pagina para cadastro de produtos

--- gerenciador/db/schema.rb
+++ gerenciador/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170617205352) do
+ActiveRecord::Schema.define(version: 20170619233156) do
+
+  create_table "products", force: :cascade do |t|
+    t.string "nome"
+    t.integer "quantidade"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "nome"
